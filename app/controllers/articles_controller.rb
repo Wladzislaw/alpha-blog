2c0c8f779f73817d1654d68b84b93201c37a521a
Wladzislaw.E.Pewzner <wladzislaw@yandex.ru>
2018-12-09 18:30:22 +0300

    Show and edit action added. Index added. Navigation between pages (except root) added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
@@ -17,6 +21,26 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "The article has been successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    
+  end
+
 
   private
   def article_params
