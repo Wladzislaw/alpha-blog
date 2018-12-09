ee03d2b916e99d03259484fc9d456a070ef876da
Wladzislaw.E.Pewzner <wladzislaw@yandex.ru>
2018-12-09 20:30:28 +0300

    Application controller bug fixed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-  before_action :set_article, only: [:edit, :show, :destroy, :update] 
+  before_action :set_article, only: [:edit, :show, :update, :destroy]
 
   def index
     @articles = Article.all
@@ -49,7 +49,7 @@ class ArticlesController < ApplicationController
   private
 
   def set_article
-    @@article = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   def article_params
